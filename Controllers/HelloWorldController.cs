ea3a814a47a972eba0b08a82878c2323d2740e6f
RubenBaader <rubenbaader@gmail.com>
2023-10-05 10:43:45 +0200

    Enabled read/write of type string from http request body and now returns saved string on GET request

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace FileTransfer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelloWorldController : ControllerBase
+    {
+        private readonly string FilePath = "data.txt";
+
+        [HttpGet(Name = "GetHelloWorld")]
+        public async Task<IActionResult> GetHelloWorld()
+        {
+            try
+            {
+                string FileData = await System.IO.File.ReadAllTextAsync(FilePath);
+
+                var response = new {Hello = FileData };
+
+                return Ok(response);
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPost (Name = "PostHelloWorld")]
+        public async Task<IActionResult> PostHelloWorld([FromBody] string Payload )
+        {
+            try
+            {
+                string Data = Payload.ToString();
+
+                await System.IO.File.WriteAllTextAsync( FilePath, Data );
+
+                return Ok("Data saved!");
+            }
+            catch(Exception ex)
+            {
+                return BadRequest($"Error: {ex.Message}");
+            }
+        }
+    }
+}
